28a632641ce03eccf5ec03ded7caca12bcad94b6
andrejarboe <andrejarboe@gmail.com>
2023-05-11 12:28:16 -0500

    round to 2

--- python/course1/4functions.py
+++ python/course1/4functions.py
@@ -15,6 +15,6 @@ print(f'total tax {total_tax}')
 
 # turns into 
 def calculateTax(bill, taxRate):
-    return (bill * taxRate) / 100.00
+    return round((bill * taxRate) / 100.00, 2)
 
 print(f"total tax {calculateTax(175, 15)}")
