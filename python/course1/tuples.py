5b6808b56ddae8d47ba6865ae333f5b2183aaa44
Andre <andrejarboe@gmail.com>
2023-05-12 13:42:22 -0500

    Create tuples.py

---
+++ python/course1/tuples.py
@@ -0,0 +1,12 @@
+# Tuples
+# # tuples are an immutable data structure.
+# Tuples are 
+
+myTuple = (1, 'strings', True)
+
+print(type(myTuple[1]))
+
+print(myTuple.count(True))
+
+for i in myTuple:
+    print(i)
